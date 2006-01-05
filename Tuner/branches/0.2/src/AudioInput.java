8d4960b53081c8a694bc0350b7bc0b6c0ccbef73
Paul Richards <paul.richards@gmail.com>
2006-01-05 21:36:34 +0000

    M Tuner/trunk/tuner.properties   Added Ukelele.
    A Tuner/branches
    A Tuner/branches/0.2
      Added branch for 0.2.
    
    
    
    git-svn-id: file:///home/pauldoo/Programs/svnrepo@37 8e395407-42ff-0310-94d9-ec183df6106f

---
+++ Tuner/branches/0.2/src/AudioInput.java
@@ -0,0 +1,47 @@
+/*
+
+Tuner, a simple application to help you tune your musical instrument.
+Copyright (C) 2003-2005 Paul Richards
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+
+
+import java.io.*;
+
+public class AudioInput {
+
+    private DataInputStream dis;
+    private int offset;
+    
+    
+    public AudioInput(DataInputStream dis) {
+	this.dis = dis;
+    }
+    
+    public int readSample() throws IOException {
+        int sample = offset + dis.readShort();
+	
+	if ( sample > 0 )
+	    offset --;
+	else if ( sample < 0 )
+	    offset ++;
+
+	return sample;
+    }
+
+}
+
