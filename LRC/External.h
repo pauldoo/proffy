7c12aafc8ec1d7569b8d8600395549a645b4bc60
Paul Richards <paul.richards@gmail.com>
2006-02-24 14:30:35 +0000

    Use real command line arguments and fix bug outputting to non-existent files.
    M Main.cpp
    
      As above.
    
    M External.h
    M Makefile
    
      Include and link with some boost command line options stuff.
    
    
    
    git-svn-id: file:///home/pauldoo/Programs/svnrepo@76 8e395407-42ff-0310-94d9-ec183df6106f

--- LRC/External.h
+++ LRC/External.h
@@ -7,6 +7,10 @@
 #include <string>
 #include <vector>
 
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/variables_map.hpp>
+
 #include <crypto++/md4.h>
 
 #endif
--- LRC/Main.cpp
+++ LRC/Main.cpp
@@ -6,29 +6,52 @@
 int main(int argc, char* argv[])
 {
     std::ios::sync_with_stdio(false);
+        
     try {
-        switch (argv[1][0]) {
-            case 'c':
-                {
-                    LRC::Compressor lrc(&std::cout, 512);
-                    lrc.Compress(std::cin);
-                    std::cout.flush();
-                }
-                break;
-            case 'd':
-                {
-                    std::fstream output(argv[2]);
-                    LRC::Decompressor lrc(&output);
-                    lrc.Decompress(std::cin);
-                }
-                break;
-            default:
-                throw "Invalid command line";
+        std::string output_filename;
+    
+        boost::program_options::options_description options("Command line options");
+        options.add_options()
+            ("help", "Display this help message")
+            ("compress", "Compresses stdin to stdout")
+            ("decompress", boost::program_options::value<std::string>(&output_filename), "Decompress stdin to specified output file");
+            
+        boost::program_options::variables_map variables;
+        boost::program_options::store(boost::program_options::parse_command_line(argc, argv, options), variables);
+        boost::program_options::notify(variables);
+    
+        if (variables.count("help")) {
+            std::cout << options << std::endl;
+            return 0;
+        }
+        
+        if ((variables.count("compress") + variables.count("decompress")) != 1) {
+            std::cerr << "Must specifiy exactly one compress or decompress flag" << std::endl;
+            std::cerr << options << std::endl;
+            return 1;
+        }
+    
+        if (variables.count("compress")) {
+            LRC::Compressor lrc(&std::cout, 512);
+            lrc.Compress(std::cin);
+            std::cout.flush();
+        } else if (variables.count("decompress")) {
+            std::fstream output(output_filename.c_str(), std::ios::binary | std::ios::in | std::ios::out | std::ios::trunc);
+            if (!output) {
+                throw std::string("Failed to open output file");
+            }
+            LRC::Decompressor lrc(&output);
+            lrc.Decompress(std::cin);
+        } else {
+            throw std::string("Invalid command line");
         }
     } catch (const std::exception& ex) {
         std::cerr << "Exception: " << ex.what() << std::endl;
         return 1;
     } catch (const std::string& ex) {
+        std::cerr << "Exception: " << ex << std::endl;
+        return 1;
+    } catch (const char* ex) {
         std::cerr << "Exception: " << ex << std::endl;
         return 1;
     } catch (...) {
