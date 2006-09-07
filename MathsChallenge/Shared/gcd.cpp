c57091d4aeb629c9e1eb041482e188cdfe88eb40
Paul Richards <paul.richards@gmail.com>
2006-09-07 20:54:36 +0000

    Adding project where I plan to complete some of the puzzles from: http://mathschallenge.net/index.php?section=project
    I'm using cmake to manage the code, and seperate out a shared chunk of code that
    hopefully will end up being reused. Only added GCD to the shared library, which
    isn't even used yet. Cmake simply requires *something* to exist in the library.
    
    
    A      .
    A      Solutions
    A      Solutions/problem_125.cpp
    A      Solutions/CMakeLists.txt
    A      Solutions/problem_125_sean.cpp
    A      Shared
    A      Shared/gcd.h
    A      Shared/CMakeLists.txt
    A      Shared/gcd.cpp
    A      CMakeLists.txt
    
    
    git-svn-id: file:///home/pauldoo/Programs/svnrepo@95 8e395407-42ff-0310-94d9-ec183df6106f

---
+++ MathsChallenge/Shared/gcd.cpp
@@ -0,0 +1,14 @@
+#include "Shared/gcd.h"
+
+template const unsigned int gcd<unsigned int>(const unsigned int&, const unsigned int&);
+
+template<typename T>
+const T gcd(const T& a, const T& b)
+{
+    if (b == 0) {
+        return a;
+    } else {
+        return gcd(b, a % b);
+    }
+}
+
---
+++ MathsChallenge/Shared/gcd.h
@@ -0,0 +1,3 @@
+template<typename T>
+const T gcd(const T& a, const T& b);
+
