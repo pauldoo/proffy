881e304b378387923ab6266fb900e1fbf9ac94d5
Paul Richards <paul.richards@gmail.com>
2007-06-14 14:55:56 +0000

    Now uses Java's own SecureRandom class instead of relying on Unix only /dev/random.
    git-svn-id: file:///home/pauldoo/Programs/svnrepo@185 8e395407-42ff-0310-94d9-ec183df6106f

--- Tools/GenRandom.java
+++ Tools/GenRandom.java
@@ -1,18 +1,18 @@
 import java.io.*;
+import java.security.*;
 
 public final class GenRandom
 {
     /**
         Generates a random value between 0 and 2^63 - 1.
-
-        Uses /dev/random to create a hopefully good random value.
     */
     public static void main(String[] args) throws IOException
     {
-        DataInput in = new DataInputStream(new FileInputStream("/dev/random"));
+        SecureRandom random = new SecureRandom();
+        System.out.println("Provider: " + random.getProvider());
         long randomValue = -1;
         while (randomValue < 0) {
-            randomValue = in.readLong();
+            randomValue = random.nextLong();
         }
         System.out.println(randomValue);
     }
