d86639a16b6230721336ea97a2c0e8cea95e51d7
Paul Richards <paul.richards@gmail.com>
2007-07-07 20:26:06 +0000

    Increment version number to 0.4.
    git-svn-id: file:///home/pauldoo/Programs/svnrepo@219 8e395407-42ff-0310-94d9-ec183df6106f

--- Pigeon/trunk/src/pigeon/About.java
+++ Pigeon/trunk/src/pigeon/About.java
@@ -29,7 +29,7 @@ import java.io.StringWriter;
 */
 public final class About
 {
-    public static final String VERSION = "0.3" + " (build " + getBuildId() + ")";
+    public static final String VERSION = "0.4" + " (build " + getBuildId() + ")";
     public static final String TITLE = "RacePoint v" + VERSION;
     public static final String CREDITS = "Created by Paul Richards <paul.richards@gmail.com>.";
     
