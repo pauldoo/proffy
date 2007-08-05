482b3e26abb6605f736ab7c2cf0507e04557b2e3
Paul Richards <paul.richards@gmail.com>
2007-08-05 08:51:46 +0000

    Editing distances for a racepoint now displays each member's club and section.
    git-svn-id: file:///home/pauldoo/Programs/svnrepo@234 8e395407-42ff-0310-94d9-ec183df6106f

--- Pigeon/trunk/src/pigeon/view/DistancesTableModel.java
+++ Pigeon/trunk/src/pigeon/view/DistancesTableModel.java
@@ -21,6 +21,7 @@ import java.util.Map;
 import java.util.Set;
 import javax.swing.table.AbstractTableModel;
 import pigeon.model.Distance;
+import pigeon.model.Member;
 
 /**
  * Generic final class to edit distances associated with a list of objects (members or racepoints).
@@ -77,7 +78,20 @@ final class DistancesTableModel<Target> extends AbstractTableModel {
 
         switch (column) {
             case 0:
-                return entry.getKey();
+                Target t = entry.getKey();
+                if (t instanceof Member) {
+                    Member m = (Member)t;
+                    StringBuffer result = new StringBuffer(m.getName());
+                    if (m.getClub() != null) {
+                        result.append(", " + m.getClub());
+                    }
+                    if (m.getSection() != null) {
+                        result.append(", " + m.getSection());
+                    }
+                    return result.toString();
+                } else {
+                    return entry.getKey();
+                }
             case 1:
                 return entry.getValue().getMiles();
             case 2:
