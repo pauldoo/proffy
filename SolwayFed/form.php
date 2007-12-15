dba480e188e1bb524bcec6d42a01c95be125f8d5
Paul Richards <paul.richards@gmail.com>
2007-12-15 21:29:56 +0000

    Added function to add new event entries.  Doesn't execute the SQL statement yet, only prints it.
    git-svn-id: file:///home/pauldoo/Programs/svnrepo@281 8e395407-42ff-0310-94d9-ec183df6106f

---
+++ SolwayFed/form.php
@@ -0,0 +1,72 @@
+<form action="/test.php?action=add" method="post" enctype="multipart/form-data" id="addRacepoint">
+    <label for="racepointName">Racepoint Name:</label>
+    <input type="text" name="racepointName" id="racepointName" />
+    <label for="latitude">Latitude</label>
+    <input type="text" name="latitude" id="latitude" />
+    <label for="longitude">Longitude:</label>
+    <input type="text" name="longitude" id="longitude" />
+    <label for="details">Details:</label>
+    <textarea name="details" id="details"></textarea>
+    <label for="day">Date:</label>
+    <select name="day">
+        <option>1</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+        <option>5</option>
+        <option>6</option>
+        <option>7</option>
+        <option>8</option>
+        <option>9</option>
+        <option>10</option>
+        <option>11</option>
+        <option>12</option>
+        <option>13</option>
+        <option>14</option>
+        <option>15</option>
+        <option>16</option>
+        <option>17</option>
+        <option>18</option>
+        <option>19</option>
+        <option>20</option>
+        <option>21</option>
+        <option>22</option>
+        <option>23</option>
+        <option>24</option>
+        <option>25</option>
+        <option>26</option>
+        <option>27</option>
+        <option>28</option>
+        <option>29</option>
+        <option>30</option>
+        <option>31</option>
+     </select>
+     <select name="month" id="month">
+        <option value="1">January</option>
+        <option value="2">February</option>
+        <option value="3">March</option>
+        <option value="4">April</option>
+        <option value="5">May</option>
+        <option value="6">June</option>
+        <option value="7">July</option>
+        <option value="8">August</option>
+        <option value="9">September</option>
+        <option value="10">October</option>
+        <option value="11">November</option>
+        <option value="12">December</option>
+      </select>
+      <select name="year" id="year">
+        <option>2008</option>
+        <option>2009</option>
+        <option>2010</option>
+        <option>2011</option>
+        <option>2012</option>
+        <option>2013</option>
+        <option>2014</option>
+        <option>2015</option>
+     </select>
+     <label for="imageUpload">Image:</label>
+     <input type="file" name="imageUpload" id="imageUpload" />
+
+     <input type="submit" class="submit" />
+ </form>
--- SolwayFed/test.php
+++ SolwayFed/test.php
@@ -1,10 +1,13 @@
 <?php
-
+/**
+    Connect to the MySQL server, switch to the correct database,
+    and assert that everything worked.
+*/
 function csConnect()
 {
     include "config.php";
 
-    $dbh = mysql_connect($dbhost, $dbuser, $dbpassword) or
+    $dbh = mysql_pconnect($dbhost, $dbuser, $dbpassword) or
         die("mysql_connect failed: " . mysql_error());
 
     mysql_select_db($dbname, $dbh) or
@@ -13,43 +16,93 @@ function csConnect()
     return $dbh;
 }
 
+/**
+    Execute an SQL statement and assert success.
+*/
 function csExecuteStatement($dbh, $statement)
 {
     mysql_query($statement) or
         die("mysql_query failed: " . mysql_error());
 }
 
+/**
+    Delete existing tables and recreate empty.
+*/
 function csCreateTables($dbh)
 {
     csExecuteStatement($dbh, "DROP TABLE IF EXISTS csEvents;");
     csExecuteStatement($dbh,
         "CREATE TABLE csEvents (
             id INTEGER NOT NULL AUTO_INCREMENT,
-            racepoint VARCHAR(200),
-            date DATE,
-            mapLatitude REAL,
-            mapLongitude REAL,
-            info TEXT,
-            image MEDIUMBLOB,
-            imageMimeType VARCHAR(100),
+            racepoint VARCHAR(200) NOT NULL,
+            date DATE NOT NULL,
+            latitude REAL NOT NULL,
+            longitude REAL NOT NULL,
+            details TEXT NOT NULL,
+            imageFilename VARCHAR(200),
             PRIMARY KEY (id),
             UNIQUE KEY (date));");
+
+    echo 'Tables created.';
 }
 
-$dbh = csConnect();
+/**
+    Read the $_POST variable and insert the new event.
+*/
+function csAddEvent($dbh)
+{
+    $racepointName = $_POST["racepointName"];
+    $latitude = $_POST["latitude"] + 0.0;
+    $longitude = $_POST["longitude"] + 0.0;
+    $details = $_POST["details"];
 
-$action = $_GET['action'];
+    $day = $_POST["day"];
+    $month = $_POST["month"];
+    $year = $_POST["year"];
+    $date = mktime(0, 0, 0, $month, $day, $year);
 
-switch($action) {
-    case "view":
-        break;
-    case "install":
-        csCreateTables($dbh);
-        break;
-    default:
-        die("Unknown action: " . $action);
+    $statement = "INSERT INTO csEvents (racepoint, date, latitude, longitude, details) VALUES(".
+        "\"" . mysql_real_escape_string($racepointName) . "\", " .
+        $date . ", " .
+        $latitude . ", " .
+        $longitude . ", " .
+        "\"" . mysql_real_escape_string($details) . "\");";
+
+    echo $statement;
+
+    /*
+    if (substr($_FILES["imageUpload"]["type"], 0, 6) == "image/") {
+        move_uploaded_file($_FILES["file"]["tmp_name"], "upload/" . $_FILES["file"]["name"]);
+    }
+    */
 }
 
-echo "OK\n";
+/**
+    Entry point.
+*/
+function csMain()
+{
+    date_default_timezone_set("Europe/London") or
+        die("date_default_timezone_set failed");
+
+    $dbh = csConnect();
+
+    $action = $_GET['action'];
+    switch($action) {
+        case "add":
+            csAddEvent($dbh);
+            break;
+        case "install":
+            csCreateTables($dbh);
+            break;
+        case "view":
+            break;
+        default:
+            die("Unknown action: " . $action);
+    }
+}
+
+csMain();
+
 ?>
 
