c04be784f5a0600f745ef47df847ec22d028a8a1
Paul Richards <paul.richards@gmail.com>
2007-12-15 18:39:51 +0000

    Added 'install' action which creates tables etc.
    git-svn-id: file:///home/pauldoo/Programs/svnrepo@280 8e395407-42ff-0310-94d9-ec183df6106f

--- SolwayFed/test.php
+++ SolwayFed/test.php
@@ -1,25 +1,50 @@
 <?php
 
-function sf_connect()
+function csConnect()
 {
-    include 'config.php';
+    include "config.php";
 
     $dbh = mysql_connect($dbhost, $dbuser, $dbpassword) or
-        die('mysql_connect failed: ' . mysql_error());
+        die("mysql_connect failed: " . mysql_error());
 
     mysql_select_db($dbname, $dbh) or
-        die('mysql_select_db failed: ' . mysql_error());
+        die("mysql_select_db failed: " . mysql_error());
 
     return $dbh;
 }
 
-$dbh = sf_connect();
+function csExecuteStatement($dbh, $statement)
+{
+    mysql_query($statement) or
+        die("mysql_query failed: " . mysql_error());
+}
+
+function csCreateTables($dbh)
+{
+    csExecuteStatement($dbh, "DROP TABLE IF EXISTS csEvents;");
+    csExecuteStatement($dbh,
+        "CREATE TABLE csEvents (
+            id INTEGER NOT NULL AUTO_INCREMENT,
+            racepoint VARCHAR(200),
+            date DATE,
+            mapLatitude REAL,
+            mapLongitude REAL,
+            info TEXT,
+            image MEDIUMBLOB,
+            imageMimeType VARCHAR(100),
+            PRIMARY KEY (id),
+            UNIQUE KEY (date));");
+}
+
+$dbh = csConnect();
 
 $action = $_GET['action'];
 
 switch($action) {
     case "view":
-        ?>BOO<?php
+        break;
+    case "install":
+        csCreateTables($dbh);
         break;
     default:
         die("Unknown action: " . $action);
