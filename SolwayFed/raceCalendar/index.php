c01c94d6449db260424e7b8a03e7b3ec79b44bf5
Paul Richards <paul.richards@gmail.com>
2008-01-21 20:09:22 +0000

    Adding the calendar script that Sarah created, and to which I'll add the calls out to the database.
    git-svn-id: file:///home/pauldoo/Programs/svnrepo@299 8e395407-42ff-0310-94d9-ec183df6106f

---
+++ SolwayFed/raceCalendar/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><!-- InstanceBegin template="/Templates/general.dwt" codeOutsideHTMLIsLocked="false" -->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>The Solway Federation of Racing Pigeon Societies - Race Calendar</title>
+<!-- InstanceEndEditable -->
+<link rel="stylesheet" type="text/css" href="/css/solwayFed.css" />
+<!--[if IE]>
+<link rel="stylesheet" type="text/css" href="/css/ieFixes.css" />
+<!-[end if]-->
+<link rel="stylesheet" type="text/css" href="/css/solwayFed_print.css" media="print" />
+<script type="text/javascript" src="/scripts/solwayFed.js"></script>
+<!-- InstanceBeginEditable name="head" --><!-- InstanceEndEditable -->
+</head>
+
+<body onload="heights();" onresize="heights();" id="body">
+	<div id="topBanner">
+		<div id="search">
+			 <? include("c:\sites_web\sarah_solwayFed\includes\search.php"); ?>
+		</div>
+		<div id="logo">
+			<? include("c:\sites_web\sarah_solwayFed\includes\logo.php"); ?>
+		</div>
+		<div id="banner">
+			  <? include("c:\sites_web\sarah_solwayFed\includes\banner.php"); ?>
+		</div>
+	</div>
+	<div id="nav">
+		<? include("c:\sites_web\sarah_solwayFed\includes\\navigation.php"); ?>
+	</div>
+	<div id="belowNav">
+		<div id="contentArea">
+			<div id="leftCol"><!-- InstanceBeginEditable name="EditRegion3" -->
+			  <h1>Race Calendar</h1>
+			  <div class="race">
+ 			  	<a href="#"><img src="../images/image2.jpg" alt="raceImage"/></a>
+				<p><span><strong>Location:</strong></span> Shap<br/>
+				<span><strong>Date:</strong></span> 01/01/08</p>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris a odio at felis gravida tincidunt. Vestibulum vitae. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris a odio at felis gravida tincidunt. Vestibulum vitae. <a href="#">read more...</a></p>
+			  </div>
+			  <hr/>
+			  <div class="race">
+ 			  	<a href="#"><img src="../images/image2.jpg" alt="raceImage"/></a>
+				<p><span><strong>Location:</strong></span> Shap<br/>
+				<span><strong>Date:</strong></span> 01/01/08</p>
+				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris a odio at felis gravida tincidunt. Vestibulum vitae. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris a odio at felis gravida tincidunt. Vestibulum vitae. <a href="#">read more...</a></p>
+			  </div>
+			<!-- InstanceEndEditable --></div>
+			<div id="rightCol">
+			   <h1>This Week's Race</h1>
+			   <img src="/images/image2.jpg" alt="Image" />
+			   <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris a odio at felis gravida tincidunt. Vestibulum vitae. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris a odio at felis gravida tincidunt. Vestibulum vitae. <a href="#">more...</a></p>
+			</div>
+		</div>
+	</div>
+	<div id="footer">
+		  <? include("c:\sites_web\sarah_solwayFed\includes\footer.php"); ?>
+	</div>
+</body>
+<!-- InstanceEnd --></html>
