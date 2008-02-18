54a669a0d51612b7ec1ae075bde807b07995fad5
Paul Richards <paul.richards@gmail.com>
2008-02-18 09:14:02 +0000

    Adding sample app for using MT19937 random number generator from boost.
    git-svn-id: file:///home/pauldoo/Programs/svnrepo@309 8e395407-42ff-0310-94d9-ec183df6106f

---
+++ MT19937/main.cpp
@@ -0,0 +1,12 @@
+#include <boost/random/mersenne_twister.hpp>
+#include <iostream>
+
+int main(void)
+{
+    boost::mt19937 generator;
+    for (int i = 0; i < 1000; i++) {
+        std::cout << generator() << ((i % 5 == 4) ? "\n" : " ");
+    }
+    return 0;
+}
+
