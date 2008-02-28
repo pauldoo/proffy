4bc63f546721cebcef0f96de74f3b0d2841e21e3
Paul Richards <paul.richards@gmail.com>
2008-02-28 21:02:33 +0000

    New script to show next upcoming race.
    git-svn-id: file:///home/pauldoo/Programs/svnrepo@317 8e395407-42ff-0310-94d9-ec183df6106f

---
+++ SolwayFed/includes/next.php
@@ -0,0 +1,35 @@
+<?php
+require "../utils.php";
+
+$dbh = csConnect();
+$result = csExecuteQuery($dbh,
+    "SELECT * FROM csEvents WHERE date >= CURRENT_DATE() ORDER BY date LIMIT 0,1");
+
+if ($row = mysql_fetch_assoc($result)) {
+    $friendlyDate = date('d/m/Y', strtotime($row["date"]));
+?>
+
+    <h1>This Week's Race</h1>
+    <a href="#"><img src="../<?php echo htmlspecialchars($row["imageFilename"]); ?>" alt="raceImage"/></a>
+    <p><?php echo htmlspecialchars($row["details"]); ?><br/>
+    <a href="#">read more...</a></p>
+
+    <!--
+    <div class="race">
+    <a href="#"><img src="../<?php echo htmlspecialchars($row["imageFilename"]); ?>" alt="raceImage"/></a>
+    <p><span><strong>Location:</strong></span><?php echo htmlspecialchars($row["racepoint"]); ?><br/>
+    <span><strong>Date:</strong></span><?php echo htmlspecialchars($friendlyDate); ?></p>
+    <p><?php echo htmlspecialchars($row["details"]); ?><br/>
+    <a href="#">read more...</a></p>
+    </div>
+    -->
+
+<?php
+} else {
+?>
+<p>None!</p>
+
+<?php
+}
+?>
+
