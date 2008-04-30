df0f681cc1783e1aafaaff8cf0676ce1731ce53e
Paul Richards <paul.richards@gmail.com>
2008-04-30 21:36:37 +0000

    DLA fractal is now (minimally) interactive.  The user can draw fixed points onto the canvas.  I'm not sure if I'm happy with this yet.
    git-svn-id: file:///home/pauldoo/Programs/svnrepo@359 8e395407-42ff-0310-94d9-ec183df6106f

--- Fractals/src/fractals/DiffusionLimitedAggregation.java
+++ Fractals/src/fractals/DiffusionLimitedAggregation.java
@@ -19,41 +19,43 @@ package fractals;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.event.MouseEvent;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Point2D;
 import javax.swing.JComponent;
+import javax.swing.event.MouseInputListener;
 
-final class DiffusionLimitedAggregation extends BackgroundRenderingComponent
+final class DiffusionLimitedAggregation
 {
-    private static final long serialVersionUID = 3769090052648725711L;
-    
     private static final double PARTICLE_RADIUS = 0.25;
     private static final double RELEASE_GAP = PARTICLE_RADIUS * 10;
     private static final double STICKINESS = 1.0;
     
-    DiffusionLimitedAggregation()
-    {
-    }
+    private PointSet pointSet;
+    private Graphics2D graphics;
+    private final double width;
+    private final double height;
+    private final Point2D.Double center;
+    private double currentReleaseRadius = RELEASE_GAP;        
     
     static JComponent createView()
     {
-        return new DiffusionLimitedAggregation();
+        return new DiffusionLimitedAggregationComponent();
     }
     
-    @Override
-    protected void render(Graphics2D g) throws InterruptedException
+    DiffusionLimitedAggregation(Graphics2D g, double width, double height)
     {
-        Utilities.setGraphicsToHighQuality(g);
-        g.setColor(Color.RED);
-        PointSet pointSet = new QuadTreePointSet();
-        
-        final double width = getWidth();
-        final double height = getHeight();
-        final Point2D.Double center = new Point2D.Double(width/2, height/2);
-        pointSet = fixatePoint(center, g, pointSet);
-        
-        double currentReleaseRadius = RELEASE_GAP;        
-        
+        this.pointSet = new QuadTreePointSet();
+        this.graphics = g;
+        this.width = width;
+        this.height = height;
+        this.center = new Point2D.Double(width/2, height/2);
+
+        fixatePoint(center);
+    }
+
+    void render() throws InterruptedException
+    {
         while (currentReleaseRadius <= Math.min(width, height) / 2) {
             Point2D.Double p = generateRandomStartPoint(center, currentReleaseRadius);
             while (true) {
@@ -66,10 +68,7 @@ final class DiffusionLimitedAggregation extends BackgroundRenderingComponent
                 
                 if (closestPointDistance <= PARTICLE_RADIUS * 2.0) {
                     if (Math.random() < STICKINESS) {
-                        pointSet = fixatePoint(p, g, pointSet);
-                        currentReleaseRadius = Math.max(
-                                currentReleaseRadius,
-                                p.distance(center) + RELEASE_GAP);
+                        fixatePoint(p);
                         break;
                     }
                 }
@@ -85,10 +84,17 @@ final class DiffusionLimitedAggregation extends BackgroundRenderingComponent
         }
     }
     
-    private static PointSet fixatePoint(Point2D.Double point, Graphics2D g, PointSet pointSet)
+    synchronized void fixatePoint(Point2D.Double point)
     {
-        g.fill(new Ellipse2D.Double(point.getX() - PARTICLE_RADIUS, point.getY() - PARTICLE_RADIUS, PARTICLE_RADIUS * 2, PARTICLE_RADIUS * 2));
-        return pointSet.add(point);
+        Point2D.Double closestExisting = pointSet.findClosest(point);
+        if (closestExisting == null || closestExisting.distance(point) > 1e-6) {
+            graphics.setColor(Color.RED);
+            graphics.fill(new Ellipse2D.Double(point.getX() - PARTICLE_RADIUS, point.getY() - PARTICLE_RADIUS, PARTICLE_RADIUS * 2, PARTICLE_RADIUS * 2));
+            pointSet = pointSet.add(point);
+            currentReleaseRadius = Math.max(
+                    currentReleaseRadius,
+                    point.distance(center) + RELEASE_GAP);
+        }
     }
     
     private static Point2D.Double generateRandomStartPoint(Point2D.Double center, double releaseRadius)
@@ -111,6 +117,78 @@ final class DiffusionLimitedAggregation extends BackgroundRenderingComponent
             return new Point2D.Double(center.getX() + dx, center.getY() + dy);
         } else {
             return point;
+        }
+    }
+}
+
+final class DiffusionLimitedAggregationComponent extends BackgroundRenderingComponent
+{
+    private static final long serialVersionUID = 3769090052648725711L;
+    
+    private DiffusionLimitedAggregation dla = null;
+
+    DiffusionLimitedAggregationComponent()
+    {
+        addMouseListener(new InputHandler());
+        addMouseMotionListener(new InputHandler());
+    }
+    
+    @Override
+    protected void render(Graphics2D g) throws InterruptedException
+    {
+        Utilities.setGraphicsToHighQuality(g);
+        
+        try {
+            dla = new DiffusionLimitedAggregation(g, getWidth(), getHeight());
+            dla.render();
+        } finally {
+            dla = null;
+        }
+    }
+    
+    private void addPointAtLocation(Point2D.Double p)
+    {
+        final DiffusionLimitedAggregation localDla = this.dla;
+        if (localDla != null) {
+            localDla.fixatePoint(p);
+            repaint();
+        }
+    }
+    
+    final class InputHandler implements MouseInputListener
+    {
+        public void mouseClicked(MouseEvent e)
+        {
+        }
+
+        public void mousePressed(MouseEvent e)
+        {
+            if (e.getButton() == MouseEvent.BUTTON1) {
+                addPointAtLocation(new Point2D.Double(e.getPoint().getX(), e.getPoint().getY()));
+            }
+        }
+
+        public void mouseReleased(MouseEvent e)
+        {
+        }
+
+        public void mouseEntered(MouseEvent e)
+        {
+        }
+
+        public void mouseExited(MouseEvent e)
+        {
+        }
+
+        public void mouseDragged(MouseEvent e)
+        {
+            if (e.getButton() == MouseEvent.BUTTON1) {
+                addPointAtLocation(new Point2D.Double(e.getPoint().getX(), e.getPoint().getY()));
+            }
+        }
+
+        public void mouseMoved(MouseEvent e)
+        {
         }
     }
 }
