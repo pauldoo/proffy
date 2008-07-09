58cbfbd53858b43029ad7965d1102b6d2a7c41e1
Paul Richards <paul.richards@gmail.com>
2008-07-09 19:11:25 +0000

    Different override hack that involves replacing the entire Foobar function.
    git-svn-id: file:///home/pauldoo/Programs/svnrepo@387 8e395407-42ff-0310-94d9-ec183df6106f

---
+++ MallocOverride/MyApp/FoobarOverride.cpp
@@ -0,0 +1,11 @@
+#include "../MyLib/Foobar.h"
+
+#include <iostream>
+
+namespace MyLib
+{
+	void Foobar(void)
+	{
+		std::cout << "HACKED!\n";
+	}
+}
--- MallocOverride/MyApp/Main.cpp
+++ MallocOverride/MyApp/Main.cpp
@@ -3,26 +3,6 @@
 #include <iostream>
 #include <Windows.h>
 
-extern "C" {
-	void* malloc(size_t size)
-	{
-		printf("%s\n", __FUNCTION__);
-		return LocalAlloc(LMEM_FIXED, size);
-	}
-
-	void free(void *ptr)
-	{
-		printf("%s\n", __FUNCTION__);
-		LocalFree(ptr);
-	}
-
-	void* realloc(void *ptr, size_t size)
-	{
-		printf("%s\n", __FUNCTION__);
-		return LocalReAlloc(ptr, size, 0);
-	}
-}
-
 int main(void)
 {
 	std::cout << "Started\n";
