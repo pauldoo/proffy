c5f27e959ea922ea4757188dd8e0f8bcbb8fc158
Paul Richards <paul.richards@gmail.com>
2008-07-20 10:02:57 +0000

    First cut bifurcation diagram fractal.
    git-svn-id: file:///home/pauldoo/Programs/svnrepo@392 8e395407-42ff-0310-94d9-ec183df6106f

---
+++ Fractals/src/fractals/BifurcationDiagram.java
@@ -0,0 +1,70 @@
+/*
+    Copyright (C) 2008  Paul Richards.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package fractals;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import javax.swing.JComponent;
+
+final class BifurcationDiagram extends BackgroundRenderingComponent
+{
+    private static final long serialVersionUID = 6987190714076485299L;
+    
+    private final double[] controlPointValues = defaultControlPointValues();
+    
+    static JComponent createComponent()
+    {
+        return new BifurcationDiagram();
+    }
+
+    @Override
+    protected void render(Graphics2D g) throws InterruptedException
+    {
+        g.setBackground(Color.BLACK);
+        g.clearRect(0, 0, getWidth(), getHeight());
+        super.bufferIsNowOkayToBlit();
+        g.setColor(Color.WHITE);
+        
+        final InterpolatingCubicSpline spline = new InterpolatingCubicSpline(controlPointValues);
+        for (int xInt = 0; xInt < getWidth(); xInt++) {
+            final double x = ((xInt + 0.5) / getWidth()) * 1.2 + 2.8;
+            double y = 0.5;
+            for (int i = 0; i < 1000; i++) {
+                y = x * spline.sample(y * controlPointValues.length);
+                if (i > 100) {
+                    final int yInt = (int)Math.round(y * getHeight());
+                    if (yInt >= 0 && yInt < getHeight()) {
+                        g.fillRect(xInt, yInt, 1, 1);
+                    }
+                }
+            }
+        }
+    }
+    
+    private static double[] defaultControlPointValues()
+    {
+        double[] result = new double[20];
+        for (int i = 0; i < result.length; i++) {
+            final double x = (i + 0.5) / result.length;
+            final double y = x * (1 - x);
+            result[i] = y;
+        }
+        return result;
+    }
+}
