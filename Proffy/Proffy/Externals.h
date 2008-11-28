1112f15a3a190ef3aa13c57a3eaf83e633181a11
Paul Richards <paul.richards@gmail.com>
2008-11-28 21:22:24 +0000

    Starting my Win32 profiler project.  Skeleton project files.
    git-svn-id: file:///home/pauldoo/Programs/svnrepo@436 8e395407-42ff-0310-94d9-ec183df6106f

---
+++ Proffy/Proffy/Externals.h
@@ -0,0 +1,13 @@
+#pragma once
+/*
+	I'm placing all includes of files external to this project here.
+*/
+
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
---
+++ Proffy/Proffy/Main.cpp
@@ -0,0 +1,7 @@
+#include "Externals.h"
+
+int main(void)
+{
+	::Sleep(100);
+	return EXIT_SUCCESS;
+}
