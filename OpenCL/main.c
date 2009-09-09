593a94e2b0ec31cea0211a44dd5fef0531d1c0c2
Paul Richards <paul.richards@gmail.com>
2009-09-09 20:05:57 +0000

    Measure RMS error of result.  Vanilla software version temporarily has an error of 0.0.
    git-svn-id: file:///home/pauldoo/Programs/svnrepo@558 8e395407-42ff-0310-94d9-ec183df6106f

--- OpenCL/main.c
+++ OpenCL/main.c
@@ -248,7 +248,7 @@ static void InitializeShortVolume(const ShortVolume* const volume)
     for (z = 0; z < volume->m_count; z++) {
         for (y = 0; y < (volume->m_images + z)->m_height; y++) {
             for (x = 0; x < (volume->m_images + z)->m_width; x++) {
-                *LookupShortVolume(volume, x, y, z) = (short)(cos(sqrt(x*x + y*y + z*z) * 0.2) * 20000.0);
+                *LookupShortVolume(volume, x, y, z) = (short)round(cos(sqrt(x*x + y*y + z*z) * 0.2) * 20000.0);
             }
         }
     }
@@ -267,10 +267,57 @@ static void InitializeFloatVolume(
     for (z = 0; z < volume->m_count; z++) {
         for (y = 0; y < (volume->m_images + z)->m_height; y++) {
             for (x = 0; x < (volume->m_images + z)->m_width; x++) {
-                *LookupFloatVolume(volume, x, y, z) = (float)(cos((x + y + z) * frequencyInRadiansPerPixel * scale) * magnitude);
+                *LookupFloatVolume(volume, x, y, z) = (float)round(cos((x + y + z) * frequencyInRadiansPerPixel * scale) * magnitude);
             }
         }
     }
+}
+
+static void InitializeShortVolumeExpectedResult(
+    const ShortVolume* const volume,
+    const double frequencyInRadiansPerPixelX,
+    const double frequencyInRadiansPerPixelY,
+    const double frequencyInRadiansPerPixelZ,
+    const double magnitude)
+{
+    int x, y, z;
+
+    for (z = 0; z < volume->m_count; z++) {
+        for (y = 0; y < (volume->m_images + z)->m_height; y++) {
+            for (x = 0; x < (volume->m_images + z)->m_width; x++) {
+                const double dx = round(cos((x + y + z) * frequencyInRadiansPerPixelX) * magnitude);
+                const double dy = round(cos((x + y + z) * frequencyInRadiansPerPixelY) * magnitude);
+                const double dz = round(cos((x + y + z) * frequencyInRadiansPerPixelZ) * magnitude);
+                const double tx = x + dx;
+                const double ty = y + dy;
+                const double tz = z + dz;
+                *LookupShortVolume(volume, x, y, z) = (short)round(cos(sqrt(tx*tx + ty*ty + tz*tz) * 0.2) * 20000.0);
+            }
+        }
+    }
+}
+
+static const double MeasureRmsError(
+    const ShortVolume* const volumeA,
+    const ShortVolume* const volumeB,
+    const int ignoreRegionSize)
+{
+    double sumSquared = 0.0;
+    int count = 0;
+    int x, y, z;
+
+    for (z = ignoreRegionSize; z < (volumeA->m_count - ignoreRegionSize); z++) {
+        for (y = ignoreRegionSize; y < ((volumeA->m_images + z)->m_height - ignoreRegionSize); y++) {
+            for (x = ignoreRegionSize; x < ((volumeA->m_images + z)->m_width - ignoreRegionSize); x++) {
+                int difference =
+                    ((int)*LookupShortVolume(volumeA, x, y, z)) -
+                    ((int)*LookupShortVolume(volumeB, x, y, z));
+                sumSquared += difference*difference;
+                count++;
+            }
+        }
+    }
+    return sqrt(sumSquared / count);
 }
 
 static const char* const ReadFileIntoString(
@@ -567,6 +614,7 @@ static void Benchmark(
     const ShortVolume* const input_volume,
     const Warpfield* const warpfield,
     const ShortVolume* const output_volume,
+    const ShortVolume* const expected_volume,
     void (*func)(const ShortVolume* const, const Warpfield* const, const ShortVolume* const))
 {
     const int iterations = 1;
@@ -582,6 +630,9 @@ static void Benchmark(
     printf("\n");
     end = clock();
 
+    const double rmsError = MeasureRmsError(output_volume, expected_volume, 10);
+    printf("%s: %f rms error.\n", name, rmsError);
+
     secondsPerIteration = ((double)(end - begin)) / CLOCKS_PER_SEC / iterations;
     printf("%s: %f seconds.\n", name, secondsPerIteration);
 }
@@ -591,31 +642,42 @@ int main(void)
     const int width = 256;
     const int height = 256;
     const int depth = 256;
-    const double scale = 3;
-    ShortVolume input_volume, output_volume;
+    const double warpScale = 1;
+    const double warpFrequencyInRadiansPerPixelX = 0.1;
+    const double warpFrequencyInRadiansPerPixelY = 0.2;
+    const double warpFrequencyInRadiansPerPixelZ = 0.3;
+    const double warpMagnitude = 3.0;
+    ShortVolume input_volume, output_volume, expected_volume;
     FloatVolume warp_x, warp_y, warp_z;
     Warpfield warpfield;
 
     setvbuf(stdout, NULL, _IONBF, 0);
 
-    warpfield.m_scale = scale;
+    warpfield.m_scale = warpScale;
     warpfield.m_warp_x = &warp_x;
     warpfield.m_warp_y = &warp_y;
     warpfield.m_warp_z = &warp_z;
 
     AllocateShortVolume(&input_volume, width, height, depth);
     AllocateShortVolume(&output_volume, width, height, depth);
-    AllocateFloatVolume(&warp_x, (int)ceil(width / scale) + 1, (int)ceil(height / scale) + 1, (int)ceil(depth / scale) + 1);
-    AllocateFloatVolume(&warp_y, (int)ceil(width / scale) + 1, (int)ceil(height / scale) + 1, (int)ceil(depth / scale) + 1);
-    AllocateFloatVolume(&warp_z, (int)ceil(width / scale) + 1, (int)ceil(height / scale) + 1, (int)ceil(depth / scale) + 1);
+    AllocateShortVolume(&expected_volume, width, height, depth);
+    AllocateFloatVolume(&warp_x, (int)ceil(width / warpScale) + 1, (int)ceil(height / warpScale) + 1, (int)ceil(depth / warpScale) + 1);
+    AllocateFloatVolume(&warp_y, (int)ceil(width / warpScale) + 1, (int)ceil(height / warpScale) + 1, (int)ceil(depth / warpScale) + 1);
+    AllocateFloatVolume(&warp_z, (int)ceil(width / warpScale) + 1, (int)ceil(height / warpScale) + 1, (int)ceil(depth / warpScale) + 1);
 
     InitializeShortVolume(&input_volume);
-    InitializeFloatVolume(warpfield.m_warp_x, 0.1, 3.0, scale);
-    InitializeFloatVolume(warpfield.m_warp_y, 0.2, 3.0, scale);
-    InitializeFloatVolume(warpfield.m_warp_z, 0.3, 3.0, scale);
+    InitializeShortVolumeExpectedResult(
+            &expected_volume,
+            warpFrequencyInRadiansPerPixelX,
+            warpFrequencyInRadiansPerPixelY,
+            warpFrequencyInRadiansPerPixelZ,
+            warpMagnitude);
+    InitializeFloatVolume(warpfield.m_warp_x, warpFrequencyInRadiansPerPixelX, warpMagnitude, warpScale);
+    InitializeFloatVolume(warpfield.m_warp_y, warpFrequencyInRadiansPerPixelY, warpMagnitude, warpScale);
+    InitializeFloatVolume(warpfield.m_warp_z, warpFrequencyInRadiansPerPixelZ, warpMagnitude, warpScale);
 
-    Benchmark("Vanilla", &input_volume, &warpfield, &output_volume, WarpVanilla);
-    Benchmark("OpenCL", &input_volume, &warpfield, &output_volume, WarpOpenCL);
+    Benchmark("Vanilla", &input_volume, &warpfield, &output_volume, &expected_volume, WarpVanilla);
+    Benchmark("OpenCL", &input_volume, &warpfield, &output_volume, &expected_volume, WarpOpenCL);
 
     return 0;
 }
